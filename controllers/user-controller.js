046a7c61a8a458894a0a77fea1c96ed919cebdf5
Okens <Ng.Eric314@gmail.com>
2022-05-29 01:46:06 -0700

    Cleaning up userGetAll function.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -24,13 +24,8 @@ const userCreate = async ({body}, res) => {
 
 const userGetAll = async (req, res) => {
   try {
-    // const {username, email} = body;
-    // if (username === "" || email === "") {
-    //   res.status(400).json({message: "Missing username and/or email."})
-    //   return;
-    // }
     const userData = await User.find({})
-    console.log("userData: ",userData )
+    // console.log("userData: ",userData )
     res.status(200).json({data: userData})
       
    } catch (error) {
